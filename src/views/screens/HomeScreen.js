61a1ddc446a9b96e8b1b431f37b4662c5b4111d8
Muhammad Faizan <faizansohail077@gmail.com>
2021-04-01 11:57:58 +0500

    working on HomeScreen

--- src/views/screens/HomeScreen.js
+++ src/views/screens/HomeScreen.js
@@ -18,11 +18,34 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import COLORS from '../../consts/colors';
 import categories from '../../consts/categories';
 import foods from '../../consts/foods';
+import { useState } from 'react';
+
 const { width } = Dimensions.get('screen');
 const cardWidth = width / 2 - 20;
 
 const HomeScreen = ({ navigation }) => {
-
+  const [selectCardIndex, setSelectCardIndex] = useState(0)
+  const ListCatagories = () => {
+    return <ScrollView showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={style.categoriesListContainer}>
+      {categories.map((category, index) => (
+        <TouchableOpacity key={category.id} onPress={() => setSelectCardIndex(index)}>
+          <View style={{ backgroundColor: selectCardIndex === index ? COLORS.primary : COLORS.secondary, ...style.categoryBtn }}>
+            <View style={style.categoryBtnImgCon}>
+              <Image style={{ width: 35, height: 40, resizeMode: 'cover' }} source={category.image} />
+            </View>
+            <Text style={{ paddingLeft: 10, fontSize: 15, fontWeight: 'bold', color: selectCardIndex === index ? COLORS.white : COLORS.primary }}>{category.name}</Text>
+          </View>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  }
+  const Card = ({ food }) => {
+    console.log("TCL ~ file: HomeScreen.js ~ line 42 ~ Card ~ food", food)
+    return <View style={style.cardContainer}>
+      <Text>{food.name}</Text>
+      <Image style={{ width: 100, height: 100 }} w source={food.image} />
+    </View>
+  }
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white, padding: 20 }}>
       <View style={style.header}>
@@ -35,6 +58,20 @@ const HomeScreen = ({ navigation }) => {
         </View>
         <Image style={{ top: -30, width: 100, height: 100, borderRadius: 50 }} source={require('../../assets/faizan.png')} />
       </View>
+      <View style={{ marginTop: 40, flexDirection: 'row', paddingHorizontal: 20 }}>
+        <View style={style.inputContainer}>
+          <Icon name="search" size={28} style={{ marginLeft: 15 }} />
+          <TextInput style={{ flex: 1, fontSize: 18 }} placeholder="Search" />
+        </View>
+        <View style={style.sortBtn}>
+          <Icon name="tune" size={28} color={COLORS.white} />
+        </View>
+      </View>
+      <View>
+
+        <ListCatagories />
+      </View>
+      <FlatList keyExtractor={item => item.id} showsVerticalScrollIndicator={false} numColumns={2} data={foods} renderItem={({ item }) => <Card food={item} />} />
     </SafeAreaView>
   );
 };
@@ -44,6 +81,56 @@ const style = StyleSheet.create({
     marginTop: 20,
     flexDirection: 'row',
     justifyContent: 'space-between'
+  },
+  inputContainer: {
+    flex: 1,
+    height: 50,
+    borderRadius: 10,
+    flexDirection: 'row',
+    backgroundColor: COLORS.light,
+    alignItems: 'center',
+  },
+  sortBtn: {
+    width: 50,
+    height: 50,
+    marginLeft: 10,
+    backgroundColor: COLORS.primary,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  categoriesListContainer: {
+    padding: 30,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+
+  },
+  categoryBtn: {
+    height: 45,
+    width: 120,
+    marginRight: 7,
+    borderRadius: 30,
+    alignItems: 'center',
+    paddingHorizontal: 5,
+    flexDirection: 'row'
+  },
+  categoryBtnImgCon: {
+    height: 35,
+    width: 35,
+    backgroundColor: COLORS.white,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardContainer: {
+    height: 220,
+    width: cardWidth,
+    marginHorizontal: 10,
+    marginBottom: 20,
+    marginTop: 50,
+    backgroundColor: COLORS.white,
+    elevation: 5,
+    borderRadius: 10,
   }
 });
 
